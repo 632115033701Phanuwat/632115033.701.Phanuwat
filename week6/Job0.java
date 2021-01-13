5965e4840a6c8a4c0094c869a8527bd45c15ec39
Phanuwat Panya <77323970+632115033701Phanuwat@users.noreply.github.com>
2021-01-14 00:00:00 +0700

    Add files via upload

---
+++ week6/Job0.java
@@ -0,0 +1,14 @@
+package week6;
+
+public class Job0 extends Person{
+    String job0;
+    public Job0(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+
+    }
+    public void job0(){
+        System.out.println(name+"my job is  "+job0);
+    }
+}
---
+++ week6/Job1.java
@@ -0,0 +1,15 @@
+package week6;
+
+public class Job1 extends Person{
+    String job1;
+    public Job1(String name, int age, String gender){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+
+    }
+    public void job1(){
+        System.out.println(name+"my job is  "+job1);
+    }
+	
+}
