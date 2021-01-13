39f9f684a8e34b13a572a2c30697246b4308e136
Phanuwat Panya <phanuwat_panya@elearning.cmu.ac.th>
2021-01-13 20:43:01 +0700

    week5

---
+++ week6/Artist.java
@@ -0,0 +1,14 @@
+package week6;
+
+public class Artist extends Person {
+    String genre;
+    public Artist(String name, int age){
+        this.name = name;
+        this.age = age;
+
+    }
+    public void playMusic(){
+        System.out.println(name+" is playing "+genre+" music.");
+    }
+    
+}
