8959401887c4538f56f0e12e30799eb3a4e4a791
Phanuwat Panya <phanuwat_panya@elearning.cmu.ac.th>
2021-02-06 17:31:56 +0700

    week9

---
+++ week9/week9/Progarm3/Cube.java
@@ -0,0 +1,15 @@
+package week9.Progarm3;
+
+class Cube extends Rectangle{
+    private int Long;
+
+    public Cube(int Long1,int Height,int Long){
+        super(Long1, Height);
+        this.Long = Long;
+    }
+
+    //overiding method
+    public String toString(){
+        return super.toString()+ "\nThis is a cube with the side of "+ Long;
+    }
+}
---
+++ week9/week9/Progarm3/Shape.java
@@ -0,0 +1,13 @@
+package week9.Progarm3;
+
+public class Shape {
+    public String toString(){
+     return "This is a Shape";
+    }
+    public static void main(String[] args) {
+        Shape  shape1 = new Cube(5, 5, 5);
+
+        System.out.println(shape1.toString());
+    }
+    
+}
